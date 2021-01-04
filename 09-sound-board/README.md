09c47a14a65279f049699d1c5fc7a9c7638a7f59
tinspham209 <tindeptrai1@gmail.com>
2021-01-04 23:49:23 +0700

    add: 09-sound-board

---
+++ 09-sound-board/README.md
@@ -0,0 +1,21 @@
+# Mini Project: Sound Board
+
+## Date: 4 - Jan - 2021
+
+### Screenshot
+
+<img src="./template-project-img.png" alt="screenshot"/>
+
+### Tech-Stack
+
+- HTML/CSS
+- JS
+
+### After this project
+
+I have improve my knowledge about:
+
+- review my knowledge in html/css
+- animation with css
+- Learn DOM Manipulation
+- handle when onClick button for stop audio and set Time audio to 0
---
+++ 09-sound-board/script.js
@@ -0,0 +1,24 @@
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+sounds.forEach((sound) => {
+	const btn = document.createElement("button");
+	btn.classList.add("btn");
+	btn.innerText = sound;
+
+	btn.addEventListener("click", () => {
+		stopSound();
+
+		document.getElementById(sound).play();
+	});
+
+	document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSound() {
+	sounds.forEach((sound) => {
+		const song = document.getElementById(sound);
+
+		song.pause();
+		song.currentTime = 0;
+	});
+}
