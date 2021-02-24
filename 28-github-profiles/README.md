57b878f3d18c4a291211301eca8cabd7be30e01a
tinspham209 <tindeptrai1@gmail.com>
2021-02-24 14:03:21 +0700

    build(28-github-profiles): Build project 28

---
+++ 28-github-profiles/README.md
@@ -0,0 +1,21 @@
+# Mini Project: Github Profiles
+
+## Date: 24 - Feb - 2021
+
+### Screenshot
+
+<img src="./template-project-img.png" alt="screenshot"/>
+
+### Tech-Stack
+
+- HTML/CSS
+- JS
+- axios
+
+### After this project
+
+I have improve my knowledge about:
+
+- Play around with API github
+- understand axios
+- fetch API with axios
---
+++ 28-github-profiles/script.js
@@ -0,0 +1,86 @@
+const APIURL = "https://api.github.com/users/";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username);
+
+    createUserCard(data);
+    getRepos(username);
+  } catch (error) {
+    if (error.response.status === 404) {
+      createErrorCard("No profile with this username");
+    }
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + "/repos?sort=created");
+
+    addReposToCard(data);
+  } catch (error) {
+    createErrorCard("Problem fetching repos");
+  }
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+    <div class="card">
+      <div>
+        <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+      </div>
+      <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+        <ul>
+          <li>${user.followers} <strong>Followers</strong></li>
+          <li>${user.following} <strong>Following</strong></li>
+          <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+
+        <div id="repos"></div>
+      </div>
+    </div>
+  `;
+
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+    <div class="card">
+      <h1>${msg}</h1>
+    </div>
+  `;
+  main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+  const reposElement = document.getElementById("repos");
+
+  repos.slice(0, 5).forEach((repo) => {
+    const repoElement = document.createElement("a");
+    repoElement.classList.add("repo");
+    repoElement.href = repo.html_url;
+    repoElement.target = "_blank";
+    repoElement.innerText = repo.name;
+
+    reposElement.appendChild(repoElement);
+  });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const user = search.value;
+
+  if (user) {
+    getUser(user);
+
+    search.value = "";
+  }
+});
